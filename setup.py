52a571d39d2cc39a4d677d664be963adc6ef0b14
Simon Conseil <contact@saimon.org>
2018-09-10 10:41:56 +0200

    Add setup and make it a true package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    raise Exception('python 3.6 or newer is required')
+
+setup(
+    name='psfrec',
+    version='0.1',
+    description='MUSE WFM-AO PSF reconstruction from SPARTA',
+    author='Simon Conseil',
+    author_email='simon.conseil@univ-lyon1.fr',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['mpdaf', 'astropy', 'scipy', 'matplotlib', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'psfrec=psfrec.__main__:main',
+        ]
+    },
+)
