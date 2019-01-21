cb48906f39cdb8abf1681fb723413ad0f660da21
Simon Conseil <contact@saimon.org>
2019-01-21 17:20:11 +0100

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 
 if sys.version_info < (3, 5):
     raise Exception('python 3.5 or newer is required')
@@ -9,23 +9,4 @@ __version__ = None
 with open('psfrec/version.py') as f:
     exec(f.read())
 
-setup(
-    name='psfrec',
-    version=__version__,
-    description='MUSE WFM-AO PSF reconstruction from SPARTA',
-    author='Simon Conseil',
-    author_email='simon.conseil@univ-lyon1.fr',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    python_requires='>=3.5',
-    install_requires=['mpdaf', 'astropy', 'scipy', 'numpy', 'joblib'],
-    extras_require={
-        'all': ['matplotlib', 'colorama'],
-    },
-    entry_points={
-        'console_scripts': [
-            'psfrec=psfrec.run_psfrec:main',
-        ]
-    },
-)
+setup()
