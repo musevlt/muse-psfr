fb1a6c28d4a95f045e385cc327cafcdcd77f1e57
Simon Conseil <contact@saimon.org>
2019-12-30 23:34:20 -0300

    Update minimum python version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 
 from setuptools import setup
 
-if sys.version_info < (3, 5):
-    raise Exception('python 3.5 or newer is required')
+if sys.version_info < (3, 6):
+    raise Exception('python 3.6 or newer is required')
 
 setup(use_scm_version=True)
